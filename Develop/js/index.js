bc86ac99a9d31a0d988e5cbfafed4c3661272af6
rainbowdyno <67548773+rainbowdyno@users.noreply.github.com>
2022-03-01 21:05:03 -0700

    commit

--- Develop/js/index.js
+++ Develop/js/index.js
@@ -1,2 +1,44 @@
 // What do we need from this javascript? 
-// r
+// run inquirer prompt to accept input from user. use that info to write to fs and populate a character sheet. 
+// wanting to write each one to its own unique html page. Probably going to have to code mt own html and css 
+
+// const inquirer = require('inquirer')
+
+
+// const promptUser = () => {
+//     return inquirer.prompt([
+//         {
+//             type: 'input',
+//             name: 'name',
+//             message: 'What is your character\'s name?'
+//         },
+//         {
+//             type: 'list',
+//             name: 'class',
+//             message: `What class is ?` // this probably won't work?
+//         },
+//         {
+//             type: 'input',
+//             name: 'name',
+//             message: 'What is your character\'s name?'
+//         },
+//         {
+//             type: 'input',
+//             name: 'name',
+//             message: 'What is your character\'s name?'
+//         },
+//     ])}
+// promptUser()
+    // .then(console.log(answers))
+
+function getAPI() {
+fetch('https://www.dnd5eapi.co/api/classes/barbarian')
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  //Using console.log to examine the data
+  console.log(data)
+})
+}
+getAPI()
